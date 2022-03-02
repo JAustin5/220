5dfea9b16e0c291354f1af5c6e257988921821bd
JAustin5 <austinj2@g.cofc.edu>
2022-03-01 21:08:44 -0500

    Working on Lab 7

---
+++ labs/lab7/lab7.py
@@ -0,0 +1,108 @@
+"""
+Name: <Jalena Austin>
+<lab7>.py
+
+Problem: <Create various programs that use different decision statements and using random
+ generating forms to show through different outputs such as Boolean or none.>
+
+Certification of Authenticity:
+<I certify that this assignment is my own work, but I discussed it with: <Ashley Woods, Isaiah Stapleton>
+"""
+import math
+from random import randint
+from graphics import *
+
+
+def get_random(move_amount):
+    rand = randint(-move_amount, move_amount)
+    return rand
+
+
+def did_collide(ball, ball_2):
+    ball_center = ball.getCenter()
+    ball_rad = ball.getRadius()
+
+    ball_2_center = ball_2.getCenter()
+    ball_2_rad = ball_2.getRadius()
+
+    distance_x = (ball_center.getX() - ball_2_center.getX()) ** 2
+    distance_y = (ball_center.getY() - ball_2_center.getY()) ** 2
+    distance_value = math.sqrt(distance_x + distance_y)
+
+    rad_value = ball_rad + ball_2_rad
+
+    if distance_value <= rad_value:
+        return True
+    else:
+        return False
+
+
+def hit_vertical(ball, win):
+    ball_center = ball.getCenter().getY()
+    ball_rad = ball.getRadius()
+
+    win_span = win.getHeight()
+
+    if ball_center + ball_rad >= win_span:
+        return True
+    if ball_center <= win_span:
+        return True
+
+
+def hit_horizontal(ball, win):
+    ball_center = ball.getCenter().getX()
+    ball_rad = ball.getRadius()
+
+    win_span = win.getWidth()
+
+    if ball_center + ball_rad >= win_span:
+        return True
+    if ball_center <= win_span:
+        return True
+
+
+def get_random_color():
+    red = randint(0, 255)
+    green = randint(0, 255)
+    blue = randint(0, 255)
+    return color_rgb(red, green, blue)
+
+
+def bumper():
+    win = GraphWin("Bumper / Lab 7", 400, 400)
+
+    circle_1 = Circle(Point(50, 200), 20)
+    circle_2 = Circle(Point(350, 200), 20)
+
+    circle_1.setFill(get_random_color())
+    circle_2.setFill(get_random_color())
+    circle_1.draw(win)
+    circle_2.draw(win)
+
+    circle_1_x = get_random(25)
+    circle_1_y = get_random(25)
+    circle_2_x = get_random(25)
+    circle_2_y = get_random(25)
+
+    while not win.checkMouse():
+        time.sleep(.5)
+        circle_1.move(circle_1_x, circle_1_y)
+        circle_2.move(circle_2_x, circle_2_y)
+        if did_collide(circle_1, circle_2):
+            circle_1_x = -circle_1_x
+            circle_1_y = -circle_1_y
+            circle_2_x = -circle_2_x
+            circle_2_y = -circle_2_y
+        if hit_horizontal(circle_1, win):
+            circle_1_x = -circle_1_x
+        if hit_horizontal(circle_2, win):
+            circle_2_x = -circle_2_x
+        if hit_vertical(circle_1, win):
+            circle_1_y = -circle_1_y
+        if hit_vertical(circle_2, win):
+            circle_2_y = -circle_2_y
+
+    win.close()
+
+
+bumper()
