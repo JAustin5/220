6ab23fe8a8737d237763238380dc69b859d6ac6a
JAustin5 <austinj2@g.cofc.edu>
2022-03-03 19:34:18 -0500

    Completed HW 7

---
+++ assignments/hw7/encryption.py
@@ -0,0 +1,23 @@
+def encode(user_message, user_key):
+    final_message = ''
+
+    for i in range(len(user_message)):
+        message_input = ord(user_message[i]) + user_key
+        final_message += chr(message_input)
+
+    return final_message
+
+
+def encode_better(user_message, user_key, file_name):
+    final_message = ''
+
+    for i in range(len(user_message)):
+        message_input = ord(user_message[i]) - 65
+        convert_key = ord(user_key[i % len(user_key)]) - 65
+        new_code = message_input + convert_key
+        shift_message = new_code % 58
+        result_char = chr(shift_message + 65)
+        final_message += result_char
+
+    message_file = open(file_name, 'a')
+    message_file.write(final_message)
--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -1,42 +1,95 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Jalena Austin>
+<Functions>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Will be able to create functions to not only write functions, but open
+ to opening text files, while allowing the user to read and/or write within these text files
+ that are opened.>
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+<I certify that this assignment is my own work, but I discussed it with: <Brooke Duvall>
 """
+
+from encryption import encode, encode_better
 
 
 def number_words(in_file_name, out_file_name):
-    pass
+    my_file = open(in_file_name, 'r')
+    word_vale = 0
+    new_file = open(out_file_name, 'w')
+
+    for line in my_file:
+        for word in line.split():
+            word_vale = word_vale + 1
+            new_file.write(str(word_vale) + " " + word + "\n")
+
+    my_file.close()
+    new_file.close()
 
 
 def hourly_wages(in_file_name, out_file_name):
-    pass
+    start_file = open(in_file_name, 'r')
+    new_file = open(out_file_name, 'w')
+
+    for line in start_file:
+        line_var = line.split()
+        for i in range(len(line_var)):
+            wage_var = eval(line_var[2])
+            hour_var = eval(line_var[3])
+            sum_pay = (wage_var * hour_var) + (1.65 * hour_var)
+        new_file.write(line_var[0] + ' ' + line_var[1] + ' ' + "{:.2f}".format(sum_pay) + '\n')
+
+    start_file.close()
+    new_file.close()
 
 
 def calc_check_sum(isbn):
-    pass
+    user_isbn = isbn.replace('-', '')
+    total = 0
+
+    for i in range(10):
+        cur_val = user_isbn[::-1]
+        user_index = eval(cur_val[i])
+        total = total + (user_index * (i + 1))
+    return total
 
 
 def send_message(file_name, friend_name):
-    pass
+    start_file = open(file_name, 'r')
+    reading_file = file_name.read()
+    new_file = open(friend_name + '.txt', 'a')
+    new_file.write(reading_file)
 
-
-def encode():
-    pass
+    start_file.close()
+    new_file.close()
 
 
 def send_safe_message(file_name, friend_name, key):
-    pass
+    start_file = open(file_name, 'r')
+    file = start_file.read()
+    file_info = file.split('\n')
+    new_friend = open(friend_name + '.txt', 'a')
+
+    for i in range(len(file_info)):
+        encoded = encode(file_info[i], key)
+        new_friend.write(encoded + '\n')
+
+    start_file.close()
+    new_friend.close()
 
 
 def send_uncrackable_message(file_name, friend_name, pad_file_name):
-    pass
+    open_file = open(file_name, 'r')
+    message = open_file.read()
+    key_file = open(pad_file_name, 'r')
+    key = key_file.read()
+    message_count = message.split()
+
+    for i in message_count:
+        encode_better(i, key, friend_name + '.txt')
+
+    open_file.close()
+    key_file.close()
 
 
 if __name__ == '__main__':
