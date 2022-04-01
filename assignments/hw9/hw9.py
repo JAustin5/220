eb0e78cbb88d32d10c0cd71a9316ae5a5be659ff
JAustin5 <austinj2@g.cofc.edu>
2022-04-01 19:27:38 -0400

    HW 9

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -56,13 +56,16 @@ def won(guessed):
     return True
 
 
+secret_word = get_random_word(get_words('words.txt'))
+
+
 def play_graphics(secret_word):
     win = GraphWin('Hangman', 650, 650)
 
     instruction_msg = Text(Point(225, 550), "Guess a letter: ")
+    instruction_msg.draw(win)
     entry_box = Entry(Point(325, 550), 5)
     entry_box.draw(win)
-    instruction_msg.draw(win)
 
     base_line = Line(Point(250, 450), Point(400, 450))
     base_line.draw(win)
@@ -78,42 +81,43 @@ def play_graphics(secret_word):
     val_guess = True
     while (tries > 0) and not won(make_hidden_secret(secret_word, user_guesses)):
         while val_guess:
-            win.getMouse()
-            user_input = entry_box.getText()
-            letters_guessed = Text(Point(317.5, 130), str(user_guesses) + user_input)
-            letters_guessed.draw(win)
             word_blanks = Text(Point(317.5, 460), (make_hidden_secret(secret_word, user_guesses)))
             word_blanks.draw(win)
+            win.getMouse()
+            user_input = entry_box.getText()
+            letters_guessed = str(user_guesses)
             if not already_guessed(user_input, user_guesses):
-                word_bank = Text(Point(475, 130), user_guesses.append(user_input + ''))
-                word_bank.setText(user_guesses)
+                word_bank = Text(Point(317.5, 120), user_guesses.append(user_input))
+                word_bank.setText(letters_guessed)
+                word_bank.getText()
                 word_bank.draw(win)
                 val_guess = False
-                if not letter_in_secret_word(user_input, secret_word) and tries == 6:
+                if not letter_in_secret_word(user_input, secret_word):
                     tries = tries - 1
-                    face_circle = Circle(Point(275, 225), 27)
-                    face_circle.draw(win)
-                elif tries == 4:
-                    body_line = Line(Point(275, 250), Point(275, 350))
-                    body_line.draw(win)
-                elif tries == 3:
-                    right_arm = Line(Point(275, 270), Point(320, 300))
-                    right_arm.draw(win)
-                elif tries == 2:
-                    left_arm = Line(Point(230, 300), Point(275, 270))
-                    left_arm.draw(win)
-                elif tries == 1:
-                    right_leg = Line(Point(275, 350), Point(325, 400))
-                    right_leg.draw(win)
-                elif tries == 0:
-                    left_leg = Line(Point(225, 400), Point(275, 350))
-                    left_leg.draw(win)
+                    if tries == 5:
+                        face_circle = Circle(Point(275, 225), 27)
+                        face_circle.draw(win)
+                    elif tries == 4:
+                        body_line = Line(Point(275, 250), Point(275, 350))
+                        body_line.draw(win)
+                    elif tries == 3:
+                        right_arm = Line(Point(275, 270), Point(320, 300))
+                        right_arm.draw(win)
+                    elif tries == 2:
+                        left_arm = Line(Point(230, 300), Point(275, 270))
+                        left_arm.draw(win)
+                    elif tries == 1:
+                        right_leg = Line(Point(275, 350), Point(325, 400))
+                        right_leg.draw(win)
+                    elif tries == 0:
+                        left_leg = Line(Point(225, 400), Point(275, 350))
+                        left_leg.draw(win)
         val_guess = True
     if won(make_hidden_secret(secret_word, user_guesses)):
-        win_game = Text(Point(317.5, 100), 'Winner!')
+        win_game = Text(Point(317.5, 90), 'Winner!')
         win_game.draw(win)
     else:
-        lost_game = Text(Point(317.5, 130), 'Sorry, you did not guess the word. The secret word was ' + secret_word)
+        lost_game = Text(Point(317.5, 90), 'Sorry, you did not guess the word. The secret word was ' + secret_word)
         lost_game.draw(win)
 
     win.getMouse()
@@ -144,6 +148,6 @@ def play_command_line(secret_word):
 
 
 if __name__ == '__main__':
-    secret_word = get_random_word(get_words('words.txt'))
+    pass
     # play_command_line(secret_word)
     # play_graphics(secret_word)
