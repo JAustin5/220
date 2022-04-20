48ee8dd22839a530978991a07b5c48818a3dd31e
JAustin5 <austinj2@g.cofc.edu>
2022-04-19 20:34:36 -0400

    Lab 13

--- labs/lab13/algorithms.py
+++ labs/lab13/algorithms.py
@@ -7,8 +7,6 @@ Problem: <Using the replacement of for loops to create methods with while loops,
 Certification of Authenticity:
 I certify that this assignment is my own work, but I discussed it with: <Ashley Woods, Isaiah Stapleton>
 """
-import math
-from graphics import *
 
 
 def read_data(filename):
